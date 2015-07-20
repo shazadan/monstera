0baca1637444d7e6672e6598fc679d03464bb60c
shazadan <shazadanawaz@gmail.com>
2015-07-19 21:10:57 -0700

    added python package basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='monstera',
+      version='0.1',
+      description='machine learning utilities'',
+      url='https://github.com/shazadan/monstera.git',
+      author='Shazada',
+      author_email='',
+      license='GPL',
+      packages=['monstera'],
+      zip_safe=False)
