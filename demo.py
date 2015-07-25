f22bab7a3c43823526e3e5d72d4c5690d0c1cee5
shazadan <shazadanawaz@gmail.com>
2015-07-24 21:16:03 -0700

    converted library to use classes

--- demo.py
+++ demo.py
@@ -1,8 +1,18 @@
 __author__ = 'shazada nawaz'
 
-from monstera import summarize
+from monstera import MLObject
 
 target_url = ("https://archive.ics.uci.edu/ml/machine-learning-"
               "databases/undocumented/connectionist-bench/sonar/sonar.all-data")
 
-summarize(target_url)
+# Instantiate class
+ml = MLObject(target_url)
+
+# output summary statistics
+ml.summarize()
+
+# split the data into traning and test datasets
+train, test = ml.train_test_split(percentage=0.4)
+
+
+
--- monstera/__init__.py
+++ monstera/__init__.py
@@ -1,21 +1,34 @@
 __author__ = 'shazada nawaz'
+
 import pandas as pd
+from sklearn import cross_validation
 
 
-def summarize(csv_file_path, header_yn=None, prefix='V'):
-    ''' Output most common summary statistics on variables in a csv file '''
+class MLObject:
 
-    #read csv into pandas data frame
-    dataframe = pd.read_csv(csv_file_path, header=header_yn, prefix=prefix)
+    def __init__(self, csv_file, header=None, prefix='V'):
+        '''
+        :param csv_file: path to csv file
+        :param header: is there a header in the csv file
+        :param prefix: if no header what literal to use for attribute names
+        :return:
+        '''
+        self.dataframe = pd.read_csv(csv_file, header=header, prefix=prefix)
 
-    #print head and tail of data frame
-    print("\nDATAFRAME HEAD")
-    print(dataframe.head())
-    print("\nDATAFRAME TAIL")
-    print(dataframe.tail())
+    def summarize(self):
+        ''' Output summary statistics on data in the csv file '''
 
-    #print summary of data frame
-    print("\nSUMMARY STATISTICS")
-    summary = dataframe.describe()
-    print(summary)
+        #print head and tail of data frame
+        print(self.dataframe.head())
+        print(self.dataframe.tail())
 
+        #print summary of attributes in data frame - both numerical and categorical
+        summary = self.dataframe.describe(include='all', percentiles=[.05, .25, .75, .95])
+        print(summary)
+
+    def train_test_split(self, percentage=1):
+        '''
+        Randomly split data into training data and test data based on passed percentage value
+        :return:
+        '''
+        return cross_validation.train_test_split(self.dataframe, test_size=0.4, random_state=0)
